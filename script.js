3a2873b4441696c18f60e612cd6781beab10bbf0
G Code <seanlfrancis@gmail.com>
2021-03-15 23:53:51 -0500

    Final Javascript with comments and better names for variables

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-// Assignment Code
+// Global Variables
 var generateBtn = document.querySelector("#generate");
-// var newPasskey = document.querySelector("#password");
+
+// Main function to generate random characters in password
 function generatePassword () {
 
-var lengthOpt = parseInt(window.prompt("Password length? (from 8 to 128)"));
-if (isNaN(lengthOpt)){
+// User Input Prompts
+var length = parseInt(window.prompt("Password length? (from 8 to 128)"));
+if (isNaN(length)){
   alert("Password length must be a number.");
   return ("Bad input. Password not generated.");
 }
-var lowerOpt = window.confirm("Do you want lower cased letters?");
-var upperOpt = window.confirm("Do you want upper cased letters?");
-var numericOpt = window.confirm("Do you want numbers?");
-var symbolOpt = window.confirm("Do you want special characters?");
+var lower = window.confirm("Do you want lower cased letters?");
+var upper = window.confirm("Do you want upper cased letters?");
+var numeric = window.confirm("Do you want numbers?");
+var symbol = window.confirm("Do you want special characters?");
 
 // Check to see if user chose at least one character type to be included into the password.
-if ((lowerOpt === false) &&
-    (upperOpt === false) &&
-    (numericOpt === false) &&
-    (symbolOpt === false)){
+if ((lower === false) &&
+    (upper === false) &&
+    (numeric === false) &&
+    (symbol === false)){
       alert("At least one character type must be chosen for password!");
-      return ("Bad input. Password not generated.");  
-    }
+      return ("Bad input. Password not generated.");};
 
+// Character types defined      
 let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numericalCharacters = "0123456789";
@@ -30,61 +32,38 @@ let availableCharacters = "";
 let countOfPswchar = 0;
 let password = "";
 
-if (lowerOpt === true){
+if (lower === true){
 availableCharacters += lowerCaseCharacters;
 password += lowerCaseCharacters[ Math.floor(Math.random() * lowerCaseCharacters.length )];
-countOfPswchar ++;
-console.log("lowerOpt", password);
-};
-if (upperOpt === true){
+countOfPswchar ++;};
+
+if (upper === true){
 availableCharacters += upperCaseCharacters;
 password += upperCaseCharacters[ Math.floor(Math.random() * upperCaseCharacters.length )];
-countOfPswchar ++;
-};
-if (numericOpt === true){
+countOfPswchar ++;};
+
+if (numeric === true){
 availableCharacters += numericalCharacters;
 password += numericalCharacters[ Math.floor(Math.random() * numericalCharacters.length )];
-countOfPswchar ++;
-};
-if (symbolOpt === true){
+countOfPswchar ++;};
+
+if (symbol === true){
 availableCharacters += specialCharacters;
 password += specialCharacters[ Math.floor(Math.random() * specialCharacters.length )];
-countOfPswchar ++;
-};
-console.log("password", password);
+countOfPswchar ++;};
 
 //Generate the rest of the password
-for (;countOfPswchar < lengthOpt; countOfPswchar++) {
-  password += availableCharacters[ Math.floor(Math.random() * availableCharacters.length )];  
-}
-console.log("fullpassword", password);
+for (;countOfPswchar < length; countOfPswchar++) {
+  password += availableCharacters[ Math.floor(Math.random() * availableCharacters.length )];};
 
-
-
-
-return password
-
-}
-
-// var arbitraryFunc = {
-//   lower: getRandomLower,
-//   upper: getRandomUpper,
-//   numeric: getRandomNumber,
-//   symbol: getRandomSymbol
-// };
+return password};
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;};
 
-  passwordText.value = password;
-
-
-
-  
-}
-
-
-// Add event listener to generate button
+// Event listener on generate button
 generateBtn.addEventListener("click", writePassword);
